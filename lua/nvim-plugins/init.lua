15605793443166589a2a8bf3af2237e50b91e491
Tom Lynch <twhlynch@gmail.com>
2025-10-06 17:20:07 +1100

    fix how plugins are setup

--- lua/nvim-plugins/init.lua
+++ lua/nvim-plugins/init.lua
@@ -1,11 +1,7 @@
 local M = {}
 
-local options = {}
-
 function M.load(name, opts)
-	opts = opts or {}
-
-	local plugin, ok = pcall(require, "nvim-plugins." .. name)
+	local plugin, ok = pcall(require, "nvim-plugins.lua.nvim-plugins." .. name)
 	if ok == true and plugin ~= nil then
 		---@diagnostic disable-next-line: undefined-field
 		if plugin ~= nil and plugin.setup ~= nil then
@@ -17,11 +13,11 @@ function M.load(name, opts)
 end
 
 function M.setup(opts)
-	options = vim.tbl_deep_extend("keep", opts or {}, options)
+	opts = opts or {}
 
 	Plugins = {}
 
-	for name, config in pairs(options) do
+	for name, config in pairs(opts) do
 		if config ~= nil and config.enabled == true then
 			M.load(name, config)
 		end
