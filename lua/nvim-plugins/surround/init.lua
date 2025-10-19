8afcd6d2933f379bbf84d80561e1d7989ceca801
Tom Lynch <twhlynch@gmail.com>
2025-10-20 09:26:57 +1100

    add normal mode surround bind

--- lua/nvim-plugins/surround/init.lua
+++ lua/nvim-plugins/surround/init.lua
@@ -18,16 +18,20 @@ local options = {
 	},
 }
 
--- surround visual selection
+-- surround selection
 function M.surround(trigger, pref, suff)
+	-- visual mode
 	local cmd = ":<C-u>normal!`>a" .. suff .. "<Esc>`<i" .. pref .. "<Esc>"
-
 	vim.keymap.set({ "v", "x" }, options.prefix .. trigger, function()
 		if vim.fn.mode() == "" then -- visual block
 			return cmd .. "`<<C-v>`>" .. string.rep("l", #suff)
 		else
 			return cmd .. "`<v`>" .. string.rep("l", #pref + #suff)
 		end
+	end, { noremap = true, silent = true, desc = "Surround selection with " .. pref .. " " .. suff, expr = true })
+	-- normal mode
+	vim.keymap.set({ "n" }, options.prefix .. trigger, function()
+		return "a" .. suff .. "<Esc>" .. string.rep("h", #suff) .. "i" .. pref .. "<Esc>l"
 	end, { noremap = true, silent = true, desc = "Surround selection with " .. pref .. " " .. suff, expr = true })
 end
 
